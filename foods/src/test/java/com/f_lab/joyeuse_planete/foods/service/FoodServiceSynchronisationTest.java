6c03af14f20a1b6ef11778368b65ce66ccec7052
koreanMike513 <paulryu1998@gmail.com>
2025-02-23 16:22:19 +0000

    fix

--- foods/src/test/java/com/f_lab/joyeuse_planete/foods/service/FoodServiceSynchronisationTest.java
+++ foods/src/test/java/com/f_lab/joyeuse_planete/foods/service/FoodServiceSynchronisationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.math.BigDecimal;
 import java.util.concurrent.CountDownLatch;
